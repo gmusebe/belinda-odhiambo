445729d387091e8434488e717da718f1afb323b3
Gmusebe <musebeivan@gmail.com>
2024-10-12 18:40:11 +0300

    Header & Navs - desktop nav

---
+++ components/Nav.jsx
@@ -0,0 +1,46 @@
+import Link from 'next/link';
+import { useContext } from 'react';
+import { cursorContext } from './cursorContext';
+import { usePathname } from 'next/navigation';
+
+const links = [
+    {
+        href: "/",
+        name: "Home",
+    },
+    {
+        href: "/about",
+        name: "About",
+    },
+    {
+        href: "/transformation",
+        name: "Transformation",
+    },
+    {
+        href: "/contact",
+        name: "Contact",
+    },
+]
+
+const Nav = () => {
+    const pathname = usePathname()
+    return <nav>
+        <div className="container mx-auto flex gap-8">
+            {links.map((link, index) => {
+                return (
+                <Link
+                href={link.href}
+                key={index}
+                className={`${
+                    pathname === link.href && "border-b-2 border-accent"
+                } uppercase`} 
+                >
+                    {link.name}
+                </Link>
+                );
+            })}
+        </div>
+    </nav>;
+};
+
+export default Nav;
