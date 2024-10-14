828c5872be76dde9bef478339588be53b7a56aa6
Gmusebe <musebeivan@gmail.com>
2024-10-15 00:23:50 +0300

    Homepage - Header

--- components/Header.jsx
+++ components/Header.jsx
@@ -11,8 +11,9 @@ import { AiOutlineMenu } from 'react-icons/ai';
 // components
 import MobileNav from './MobileNav';
 import Nav from './Nav';
-
-
+import Socials from './Socials';
+import { FaPhoneAlt } from 'react-icons/fa';
+import { IoMdMail } from 'react-icons/io';
 
 const Header = () => {
   const { mouseEnterHandler, mouseLeaveHandler } = useContext(cursorContext);
@@ -21,7 +22,27 @@ const Header = () => {
     <header className="pb-6 xl:pb-[50px] fixed z-40 w-full bg-accent-100 xl:bg-transparent">
       {/* topbar */}
       <div className="bg-secondary-100 mb-6 xl:mb-[50px] xl:h-[50px] py-4 xl:py-0">
-        topbar
+        <div className="container mx-auto h-full">
+          <div className="flex items-center justify-between h-full">
+            {/* phone & email */}
+            <div className="flex flex-col lg:flex-row items-center h-full gap-2 xl:gap-6 w-full justify-between xl:w-auto xl:justify-normal">
+              {/* phone */}
+              <div className="flex items-center gap-2 text-white">
+                <FaPhoneAlt />
+                <span>+254(0)727 661 138</span>
+              </div>
+              {/* email */}
+              <div className="flex items-center gap-2 text-white">
+                <IoMdMail />
+                <span>solutions@belshyglobal.com</span>
+              </div>
+            </div>
+            {/* socials */}
+            <div className="hidden xl:block">
+              <Socials ContainerStyles="flex gap-6 text-white"/>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="container mx-auto flex items-center justify-between px-6">
         {/* logo */}
