713b7e7ba6edfee2e1dd04b71e5fc6fc8368efed
Gmusebe <musebeivan@gmail.com>
2024-11-05 21:25:43 +0300

    About Page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -4,9 +4,11 @@ import { motion } from "framer-motion";
 import { useContext } from "react";
 import { cursorContext } from "@/components/cursorContext";
 import Image from "next/image";
+import StatsItem from "@/components/StatsItem";
 
 
 const About = () => {
+  const {mouseEnterHandler, mouseLeaveHandler} = useContext(cursorContext);
   return (
     <motion.section
      initial={{opacity:0}}
@@ -16,7 +18,16 @@ const About = () => {
     <div className="container mx-auto flex items-center pt-48 pb-12 xl:pt-32 xl:pb-0">
       <div className="w-full h-full flex flex-col xl:flex-row items-center justify-between">
         {/* image */}
-        <div className="relative w-[304px] h-[420px] xl:w-[384px] xl:h-[534px] mb-8 xl:mx-0" >
+        <motion.div
+        onMouseEnter={mouseEnterHandler}
+        onMouseLeave={mouseLeaveHandler}
+        initial={{ opacity: 0, x: -60}}
+        animate={{
+          opacity: 1,
+          x:0,
+          transition: {delay: 2, duration: 0.8, ease: "easeInOut"}
+        }}
+        className="relative w-[304px] h-[420px] xl:w-[384px] xl:h-[534px] mb-8 xl:mx-0" >
           <Image
             src="/assets/about/belindaLogo1.svg"
             fill quality={100}
@@ -24,20 +35,42 @@ const About = () => {
             alt=""
             className="object-contain"
           />
-        </div>
+        </motion.div>
         {/* text */}
-        <div className="flex flex-col items-start xl:max-w-[650px] text-center xl:text-left mx-auto xl:mx-0">
+        <motion.div
+        onMouseEnter={mouseEnterHandler}
+        onMouseLeave={mouseLeaveHandler}
+          initial={{ opacity: 0, x: 60}}
+          animate={{
+            opacity: 1,
+            x:0,
+            transition: {delay: 2.4, duration: 0.8, ease: "easeInOut"}
+          }}
+          className="flex flex-col items-start xl:max-w-[650px] text-center xl:text-left mx-auto xl:mx-0"
+        >
           <h2 className="h2 mb-6 mx-auto max-w-[540px] xl:max-w-none text-5xl"  >
-            Committed to Inspiring Change and Compassionate Service
+            Tailored Solutions for Your Success
           </h2>
-          <p>
-            I’m Belinda Odhiambo, owner of Belshy Global, dedicated to empowering and supporting individuals and
-            organizations through a diverse range of services. I am committed to helping people build resilience
-            and navigate life’s challenges with strength. My work in humanitarian diplomatic chaplaincy reflects 
-            my dedication to fostering compassion and understanding across communities. At Belshy Global, we offer
-            personalized solutions designed to make a positive impact through purposeful engagement and compassionate care.
+          <p className="lead max-w-[600px] mx-auto font-primary">
+             I am Belinda, dedicated to understanding your unique challenges and tailoring solutions that address them precisely.
+             I believe in a personalized approach that not only meets your needs but also aligns with your long-term
+             goals. Let me be your partner in navigating every step toward success.
           </p>
-        </div>
+          {/* Items */}
+          <div className="grid grid-cols-3 gap-[30px] mb-14 mx-auto xl:mx-0">
+            <div>
+              <StatsItem countNum={13} text="Years on Market" />
+            </div>
+            <div>
+              <StatsItem countNum={35} countText="k+" text="Satisfied Clients"/>
+            </div>
+            <div>
+            <StatsItem countNum={99} countText="%" text="Service Excellence"/>
+            </div>
+          </div>
+          {/* btn */}
+          <button className="btn mx-auto xl:mx-0">Contact Me</button>
+        </motion.div>
       </div>
     </div>
   </motion.section>
